2eb76947c1a29c2851bd51a7413c3efaccebde47
EugeneGpil <gpilligrim@gmail.com>
2023-08-22 03:44:33 +0000

    1

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/EugeneGpil/tester_dummy_example
 
 go 1.20
 
-require github.com/EugeneGpil/tester v0.0.0-20230821172440-bd534d7e5ede
+require github.com/EugeneGpil/tester v0.0.1
--- tester_dummy_example_test.go
+++ tester_dummy_example_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	netUrl "net/url"
 	"testing"
 
 	"github.com/EugeneGpil/tester"
@@ -25,6 +27,11 @@ func Test_assert_len(t *testing.T) {
 	tester.AssertLen(sliceOfSomeStructs, 2)
 }
 
+func Test_assert_nil(t *testing.T) {
+	tester.SetTester(t)
+	tester.AssertNil(nil)
+}
+
 func Test_assert_not_nil(t *testing.T) {
 	tester.SetTester(t)
 
@@ -38,6 +45,42 @@ func Test_assert_same(t *testing.T) {
 	tester.AssertSame("test", "test")
 	tester.AssertSame(1, 1)
 	tester.AssertSame([]byte("Hello"), []byte("Hello"))
+}
+
+func Test_assert_response_writer_message(t *testing.T) {
+	tester.SetTester(t)
+
+	helloMessage := []byte("Hello")
+
+	callback := func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write(helloMessage)
+	}
+
+	mux := http.NewServeMux()
+
+	url := "/some/url/"
+
+	mux.HandleFunc(url, callback)
+
+	urlObj, err := netUrl.Parse(url)
+	tester.AssertNil(err)
+
+	request := http.Request{
+		Method: http.MethodGet,
+		URL:    urlObj,
+	}
+
+	handler, _ := mux.Handler(&request)
+
+	writer := tester.GetTestResponseWriter()
+
+	handler.ServeHTTP(writer, &request)
+
+	tester.AssertLen(writer.GetMessages(), 1)
+
+	responseMessage := writer.GetMessages()[0]
+
+	tester.AssertSame(responseMessage, helloMessage)
 }
 
 type someStruct struct {
